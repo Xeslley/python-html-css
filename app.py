f7792fd333c19ae3584407a538d015f5bbe5f38a
Xeslley <wesleypatricksouza@gmail.com>
2021-08-19 23:29:12 +0000

    adiciona conexões ao sqlite

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, \
+    abort, render_template, flash
+import sqlite3
+
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = "pudim"
 
 app = Flask(__name__)
 # __name__ variavel do python que guarda o nome do arquivo atual
+
+app.config.from_object(__name__)
+
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+
+@app.teardown_request
+def depois_request():
+    g.bd.close()
+
 
 @app.route("/hello")
 def pagina_inicial():
     return "Hello World"
 
-@app.route("/amor")
-def pagina_amor():
-    return "<h1>Te Amo!!!</h1>"
 
